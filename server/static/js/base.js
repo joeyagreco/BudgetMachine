64bcf51bd5e4d24cc33bf8eb256636529509c316
jg <57115019+joeyagreco@users.noreply.github.com>
2021-06-03 20:52:44 -0500

    adding skeleton for html and such

---
+++ server/static/js/base.js
@@ -0,0 +1,42 @@
+function leagueHomepageRedirect() {
+    var leagueId = document.getElementById("league_id").value;
+    startLoading();
+    window.location = "/league-homepage?league_id=" + leagueId;
+}
+
+function statExplanationRedirect() {
+    var leagueId = document.getElementById("league_id").value;
+    startLoading();
+    window.location = "/stats-explained?league_id=" + leagueId;
+}
+
+function aboutRedirect() {
+    var leagueId = document.getElementById("league_id").value;
+    startLoading();
+    window.location = "/about?league_id=" + leagueId;
+}
+
+function feedbackRedirect() {
+    var leagueId = document.getElementById("league_id").value;
+    startLoading();
+    window.location = "/feedback?league_id=" + leagueId;
+}
+
+function startLoading() {
+    document.querySelector("#loader-wrapper").style.display = "flex";
+    document.querySelector("#loader-wrapper").style.visibility = "visible";
+}
+
+function stopLoading() {
+    document.querySelector("#loader-wrapper").style.display = "none";
+    document.querySelector("#loader-wrapper").style.visibility = "hidden";
+    document.querySelector("body").style.visibility = "visible";
+}
+
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        startLoading();
+    } else {
+        stopLoading();
+    }
+}
--- server/views/homepage.py
+++ server/views/homepage.py
@@ -1,6 +1,8 @@
+from flask import render_template
+
 from server.app import app
 
 
 @app.route('/')
 def homepage():
-    return "homepage"
+    return render_template("base.html")
