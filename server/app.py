4c3e29d2195fa15897fd945d4627d18a3b2a4a12
jg <57115019+joeyagreco@users.noreply.github.com>
2021-06-03 20:10:37 -0500

    added basic flask outline

--- server/app.py
+++ server/app.py
@@ -1,13 +1,7 @@
-import datetime
+from flask import Flask
 
-from server.clients.DatabaseClient import DatabaseClient
-from server.enums.Category import Category
+app = Flask(__name__)
 
 if __name__ == "__main__":
-    dbClient = DatabaseClient()
-    tId = dbClient.addTransaction(4.20, "test transaction", Category.BILL, True, datetime.date(1997, 3, 9))
-    # result = dbClient.addTransaction(None, 4.20, "test transaction", Category.BILL)
-    # result = dbClient.updateTransaction("2d885c91c40d11eba5dd00d861588225", "updatedNoteHere")
-    result = dbClient.getTransaction(tId)
-    # result = dbClient.deleteTransaction("2d885c91c40d11eba5dd00d861588225")
-    print(result)
+    from views.homepage import *
+    app.run()
