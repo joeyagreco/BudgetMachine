81d5892db12a2196d3ea41f6c0bd2c5c5de03b5b
jg <57115019+joeyagreco@users.noreply.github.com>
2021-06-06 12:58:44 -0500

    added categories to dropdown

--- server/views/homepage.py
+++ server/views/homepage.py
@@ -1,8 +1,9 @@
 from flask import render_template
 
 from server.app import app
+from server.enums.Category import Category
 
 
 @app.route('/')
 def homepage():
-    return render_template("base.html")
+    return render_template("base.html", categories=sorted([value for name, value in vars(Category).items() if name.isupper()]))
