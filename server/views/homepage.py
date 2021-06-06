38e2b86cde426c28083582d9aef7506a115a0c81
jg <57115019+joeyagreco@users.noreply.github.com>
2021-06-06 17:40:19 -0500

    updating transaction popup

--- server/views/homepage.py
+++ server/views/homepage.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import render_template
 
 from server.app import app
@@ -6,4 +8,6 @@ from server.enums.Category import Category
 
 @app.route('/')
 def homepage():
-    return render_template("base.html", categories=sorted([value for name, value in vars(Category).items() if name.isupper()]))
+    categories = sorted([value for name, value in vars(Category).items() if name.isupper()])
+    currentDate = datetime.date.today()
+    return render_template("base.html", categories=categories, currentDate=currentDate)
