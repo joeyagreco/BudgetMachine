2f2a7713a94d1e4782abe2d06bf2c5bdb1db3fb7
jg <57115019+joeyagreco@users.noreply.github.com>
2021-06-09 22:58:50 -0500

    cleanup

--- server/views/homepage.py
+++ server/views/homepage.py
@@ -11,11 +11,9 @@ from server.util import YamlProcessor
 
 @app.route('/')
 def homepage():
-    print(f"get homepage -- {randint(1,100)}")
     categories = sorted([value for name, value in vars(Category).items() if name.isupper()])
     currentDate = datetime.date.today()
     mongoDbClient = MongoDBClient()
     allTransactions = mongoDbClient.getAllTransactions()
     productionData = YamlProcessor.getVariable("PRODUCTION_DATA")
-    print(f"loading page with {productionData}")
     return render_template("homepage.html", categories=categories, currentDate=currentDate, allTransactions=allTransactions, productionData=productionData)
