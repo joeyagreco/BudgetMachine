07973cbcdb18c73dda1a663795a229179b4868b7
jg <57115019+joeyagreco@users.noreply.github.com>
2021-06-11 19:25:05 -0500

    added banks

--- server/static/js/homepage.js
+++ server/static/js/homepage.js
@@ -3,7 +3,6 @@ function initializeTable() {
         $('#transactionsTable').DataTable(
             {
                 "order": [[ 5, "desc" ]],
-                "searching": false,
                 "iDisplayLength": 25,
                 "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
             }
